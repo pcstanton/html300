446352289656bd5903aeb4d9a01935c9d7cb6261
PCS Montis <pcsmontis@outlook.com>
2019-08-19 21:33:26 -0700

    lesson 8 work

---
+++ lesson08/assignment/vue-v3/src/components/image.vue
@@ -0,0 +1,19 @@
+
+<template>
+ 	<article class="post">
+		<h3 class="post__title">{{ post.title }}</h3>
+		<h6 class="post__img">{{ post.img}}</h6>
+		<p class="post__alt">{{ post.alt }}</p>
+	
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'post',
+  props: ['post'],
+  
+}
+
+</script>
+
